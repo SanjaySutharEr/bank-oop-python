1f4e8848ea3eca62cc60804ff2a24301b199e94e
Sanjay Suthar <sutharsanjay642@gmail.com>
2026-01-03 20:38:49 +0530

    V1: working bank system with OOP, inheritance, and transfers

---
+++ Bank_account_management_system.py
@@ -0,0 +1,105 @@
+class Bank:
+    def __init__(self):
+        self.accounts = {}
+
+    def create_account(self, account):
+        self.accounts.update({account.acc_number: account.name})
+
+    def find_account(self,acc_num):
+        if(acc_num in self.accounts):
+            return True
+        else:
+            return False
+    
+    def transfer(self,acc1, acc2, amount):
+        if(amount<= acc1.get_balance()):
+            acc1.withdraw(amount)
+            acc2.deposit(amount)
+            print("succesfully transfered money")
+            print(f"Available balance in account {acc1.acc_number}: {acc1.get_balance()}")
+            print(f"Available balance in account {acc2.acc_number}: {acc2.get_balance()}")
+        else:
+            print("Insufficient balance! Can't trasfer money")
+
+    
+    def show_account_summary(self, acc):
+        print(f"Account number: {acc.acc_number}, Account holder: {acc.name}, Account type: {acc.acc_type}, Current balance: {acc.get_balance()}")
+
+
+
+
+
+class Account:
+    def __init__(self,acc_number,holder_name,balance):
+        self.acc_number = acc_number
+        self.name = holder_name
+        self.__balance = balance
+        self.transactions = [f"deposited Rs {self.__balance} "]
+
+    def deposit(self,amount):
+        if(amount<0):
+            print("you cannot deposit negative amount")
+        else:
+            self.__balance+= amount
+            self.transactions.append((f"deposited Rs {amount}"))
+    def withdraw(self, amount):
+        if(amount> self.__balance):
+            print("Transaction failed!! insufficient balance")
+        else:
+            self.__balance-=amount
+            self.transactions.append(f"Rs {amount} withdrawn")
+    def get_balance(self):
+        return self.__balance
+    
+class SavingsAccount(Account):
+    interest_rate = 0.05 #5% interest rate
+    acc_type = "SavingsAccount"
+
+    def apply_interest(self):
+        interest = self.get_balance()*self.interest_rate
+        self.deposit(interest)
+        print(f"Rs {interest} interest added")
+
+class CurrentAccount(Account):
+    overdraft_limit = 50000
+    acc_type = "CurrentAccount"
+
+    def withdraw(self,amount):
+        if(abs(self.get_balance()-amount)>self.overdraft_limit):
+            print("Overdraft limit crossed! couldn't make transaction")
+        else:
+            self._Account__balance-= amount
+            self.transactions.append(f"Rs {amount} withdrawn")
+
+    
+mybank = Bank()
+account1 = SavingsAccount("1234", "david laid", 500)
+mybank.create_account(account1)
+account2 = SavingsAccount("2345", "john mehra", 2000)
+mybank.create_account(account2)
+account3 = CurrentAccount("5647", "sara ali", 2000)
+mybank.create_account(account3)
+
+account1.deposit(2000)
+account1.deposit(2000)
+account2.deposit(5000)
+account1.deposit(2000)
+account2.withdraw(2000)
+account1.withdraw(5000)
+account3.deposit(2000)
+
+print(mybank.accounts)
+mybank.show_account_summary(account1)
+print(mybank.find_account("1234"))
+print(mybank.find_account("1111"))
+
+mybank.transfer(account1,account3, 6000)
+mybank.transfer(account1,account3, 1000)
+print(account1.get_balance())
+account1.apply_interest()
+print(account1.get_balance())
+account3.withdraw(50000)
+print(account3.get_balance())
+
+
+    
